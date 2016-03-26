e3d6d7e0d4ed4605fa38f5b0eebfdc44b5f20a86
alberto <alberto@debian>
2016-03-26 12:06:41 +0100

    toon shading v.01

--- practica1/src/resources/fshaderToon.glsl
+++ practica1/src/resources/fshaderToon.glsl
@@ -7,9 +7,33 @@
 #endif
 
 IN vec4 color;
+IN vec4 luces;
 
-void main()
-{
-    gl_FragColor = color;
+struct LightsBuffer {
+    vec4 position;
+    vec4 direction;
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+    float angle;
+};
+
+uniform LightsBuffer bufferLights[3];
+
+void main(){
+    float intensity;
+    vec4 colorv2;
+    intensity = dot(bufferLights[0].direction,normalize(color));
+
+    if (intensity > 0.95){
+        colorv2 = vec4(1.0,0.5,0.5,1.0);
+    }else if (intensity > 0.5){
+        colorv2 = vec4(0.6,0.3,0.3,1.0);
+    }else if (intensity > 0.25){
+        colorv2 = vec4(0.4,0.2,0.2,1.0);
+    }else{
+    colorv2 = vec4(0.2,0.1,0.1,1.0);
+    }
+    gl_FragColor = colorv2;
 }
 
