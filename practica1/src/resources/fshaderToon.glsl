8e85ed7c3cbc18d6c9c8918b497e662f0b3b0c09
Solero93 <solero93@gmail.com>
2016-04-11 16:16:32 +0200

    Done toon

--- practica1/src/resources/fshaderToon.glsl
+++ practica1/src/resources/fshaderToon.glsl
@@ -49,7 +49,7 @@ void main(){
     } else {
         colorv2 = vec3(0.2,0.1,0.1);
     }
-    vec3 colorFinal = colorv2 * (normalVisionAngleCos);
+    vec3 colorFinal = colorv2 * (1.0-normalVisionAngleCos);
     gl_FragColor = vec4(colorFinal[0], colorFinal[1], colorFinal[2], 1.0);
 }
 
